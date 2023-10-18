02109a56469a874cf81aa1c25a4403bab97e7105
edmundedn <edmund0neo@gmail.com>
2023-10-18 15:26:19 +0800

    new cal

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def add(numl, num2):
+    return numl + num2
+
+def subtract(numl, num2):
+    return numl- num2
+
+def multiply(numl, num2):
+#Add code here and remove pass
+    pass
+
+def divide(numl, num2):
+#add code here and remove pass
+    pass
+
+def powerOf (num, power):
+#add code here and remove pass
+    pass
